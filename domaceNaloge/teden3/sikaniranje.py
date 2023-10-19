530b009784947100a80b96c624aab20ddeb802b7
Privatech <ziga.slatnar.stagar@gmail.com>
2023-10-19 08:47:47 +0200

    Popravek obveznega dela in dodatni del

--- domaceNaloge/teden3/sikaniranje.py
+++ domaceNaloge/teden3/sikaniranje.py
@@ -13,22 +13,38 @@ while i < 9:
     ovire.append((position, position + length, i))
 print(ovire)
 
+def najkrajsaPot(x):
+    najblizja = 10
+    for cords in ovire:
+        if cords[0] <= x <= cords[1] and cords[2] < najblizja:
+            najblizja = cords[2]
+    return najblizja
+
+
 # Preveri za kolesarja
 x = 4  # stolpec
-najblizja = 10
-for cords in ovire:
-    if cords[0] <= x <= cords[1] and cords[2] < najblizja:
-        najblizja = cords[2]
-if najblizja == 10:
-    print("Prva ovira pri", najblizja)
+najblizja_ovira = najkrajsaPot(x)
+if najblizja_ovira != 10:
+    print("Prva ovira pri", najblizja_ovira)
 else:
     print("Ovira ni bila najdena")
 
 # Dodatna naloga
+# Izracunaj sirino
 sirina = 0
 for cords in ovire:
     if cords[1] > sirina:
-        sirina = cords
+        sirina = cords[1]
 
-najdalsa_pot = (0,0)
-for cords in ovire:
+najdalsa_pot = (0, 0)
+najdalse_poti = []
+# ustvari začetne dolžine z vsak stolpec
+x = 1
+while x <= sirina:
+    trenutna = (x, najkrajsaPot(x))
+    najdalse_poti.append(trenutna)
+    if trenutna[1] > najdalsa_pot[1]:
+        najdalsa_pot = trenutna
+    x += 1
+
+print(najdalsa_pot[0], ",", ("Zmaga" if najdalsa_pot[1] == 10 else najdalsa_pot[1]))
