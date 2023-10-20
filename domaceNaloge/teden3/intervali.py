4c27b53febcccd0489ff26398b316a416682d1b4
Privatech <ziga.slatnar.stagar@gmail.com>
2023-10-20 08:57:34 +0200

    Shortened

--- domaceNaloge/teden3/intervali.py
+++ domaceNaloge/teden3/intervali.py
@@ -1,36 +1,16 @@
-intervali = [
-    (12, 18),
-    (2, 5),
-    (3, 8),
-    (0, 4),
-    (15, 19),
-    (6, 9),
-    (13, 17),
-    (4, 8)
-]
-
-intervali = [tuple(int(x) for x in vrstica.split("-")) for vrstica in open("intervali112.txt")]
+intervali = [tuple(int(x) for x in vrstica.split("-")) for vrstica in open("intervali.txt")]
 
 intervali = sorted(intervali)
 
 # Združi intervale
-def combine_intervals():
-    was_modified = False
-    i = 0
-    while i < len(intervali) - 1:
-        if intervali[i + 1][0] - intervali[i][1] <= 1:
-            novi_interval = (intervali[i][0], max(intervali[i + 1][1], intervali[i][1]))
-            intervali.remove(intervali[i + 1])
-            intervali[i] = novi_interval
-            was_modified = True
-        else:
-            i += 1
-    if not was_modified:
-        return
-    combine_intervals()
-    print(len(intervali))
-
-combine_intervals()
+i = 0
+while i < len(intervali) - 1:
+    if intervali[i + 1][0] - intervali[i][1] <= 1:
+        novi_interval = (intervali[i][0], max(intervali[i + 1][1], intervali[i][1]))
+        intervali.remove(intervali[i + 1])
+        intervali[i] = novi_interval
+    else:
+        i += 1
 # Preštej vmesna števils
 
 dovoljena_stevila = 0
