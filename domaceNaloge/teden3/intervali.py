c92be3fb4502e048983fd5363afa410a538a41dc
Privatech38 <ziga.slatnar.stagar@gmail.com>
2023-10-20 12:20:40 +0200

    Combined while loops

--- domaceNaloge/teden3/intervali.py
+++ domaceNaloge/teden3/intervali.py
@@ -3,23 +3,15 @@ intervali = [tuple(int(x) for x in vrstica.split("-")) for vrstica in open("inte
 intervali = sorted(intervali)
 
 # Združi intervale
+dovoljena_stevila = 0
 i = 0
 while i < len(intervali) - 1:
-    if intervali[i + 1][0] - intervali[i][1] <= 1:
+    razlika = intervali[i + 1][0] - intervali[i][1]
+    if razlika <= 1:
         novi_interval = (intervali[i][0], max(intervali[i + 1][1], intervali[i][1]))
         intervali.remove(intervali[i + 1])
         intervali[i] = novi_interval
     else:
+        dovoljena_stevila += razlika - 1
         i += 1
-# Preštej vmesna števils
-
-dovoljena_stevila = 0
-
-i = 0
-while i < len(intervali) - 1:
-    kolicina = intervali[i + 1][0] - intervali[i][1] - 1
-    if kolicina > 0:
-        dovoljena_stevila += kolicina
-    i += 1
-
 print(dovoljena_stevila)
